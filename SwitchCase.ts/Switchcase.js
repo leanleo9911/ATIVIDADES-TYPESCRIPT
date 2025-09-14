11483b8b8ce2a0bce14a704d401bf8fade740b41
Leonardo Costa <leonardosantoscosta40@gmail.com>
2025-09-14 20:07:10 -0300

    SwitchCase

---
+++ SwitchCase.ts/Switchcase.js
@@ -0,0 +1,43 @@
+var mes = 4;
+//Switchcase
+// Estrutura switch-case para mapear números a nomes de meses
+switch (mes) {
+    case 1: // Verifica se mes é 1
+        console.log("Janeiro"); // Se for, imprime "Janeiro"
+        break; // Sai do switch
+    case 2:
+        console.log("Fevereiro");
+        break;
+    case 3:
+        console.log("Março");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Maio");
+        break;
+    case 6:
+        console.log("Junho");
+        break;
+    case 7:
+        console.log("Julho");
+        break;
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Setembro");
+        break;
+    case 10:
+        console.log("Outubro");
+        break;
+    case 11:
+        console.log("Novembro");
+        break;
+    case 12:
+        console.log("Dezembro");
+        break;
+    default: // Caso nenhum case seja correspondido
+        console.log("Mês inválido"); // Imprime "Mês inválido"
+}
